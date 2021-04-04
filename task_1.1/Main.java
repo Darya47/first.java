b4dc121a63f853a6703346d056d3ad8f5d26faa7
Darya47 <81930273+Darya47@users.noreply.github.com>
2021-04-04 23:24:45 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+        public static void main (String[] args) {
+            String login = "user";
+            String pass = "1234567";
+            if (login.equals("admin") && pass.equals("123456")) {
+                    System.out.println("Доступ разрешен для Админа");
+                }else if(login.equals("user") && pass.equals("1234567")){
+                System.out.println("Доступ pазрешен для пользователя");
+            }else{
+                    System.out.println("Доступ запрещен");
+            }
+        }
+    }
+
+
+public class Main {
+    public static void main (String[] args) {
+        String name;
+        int age;
+        String cite;
+    }
+
