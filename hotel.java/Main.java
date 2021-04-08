36a3d03c792a0d5a1aeb5aa1b92e89cc697c3ff0
Darya47 <81930273+Darya47@users.noreply.github.com>
2021-04-08 18:34:20 +0300

    Create Main.java

---
+++ hotel.java/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Hotel marriott = new Hotel(new Room[]
+                {
+                        new Room(2, true, false, false, false, 11),
+                        new Room(2, true, true, false, false, 12),
+                        new Room(3, true, true, true, false, 13),
+                        new Room(3, true, true, true, true, 21),
+                        new Room(4, true, true, false, false, 22),
+                        new Room(2, false, false, false, false, 23),
+                        new Room(2, false, true, false, false, 31),
+                        new Room(1, false, true, true, true, 32),
+                        new Room(1, true, true, true, true, 33),
+                });
+        while (true) {
+            System.out.println("Введите команду");
+            String command = scanner.nextLine();
+            if (command.equals("eat")) {
+                marriott.getFreeRoomsWithEat();
+            } else if (command.equals("freeRooms")) {
+                marriott.getFreeRooms();
+            } else if (command.equals("wifi")){
+                marriott.getFreeRoomsWithWifi();
+            } else if (command.equals("toilet")){
+                marriott.getFreeRoomsWithToilet();
+            } else if (command.equals("shower")){
+                marriott.getFreeRoomsWithShower();
+            }
+        }
+    }
+}
