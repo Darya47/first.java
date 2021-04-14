e0a65ab60df3e7fcc3cb1a5758f4ee795e6b7a82
Darya47 <81930273+Darya47@users.noreply.github.com>
2021-04-14 23:41:46 +0300

    Create MultiThread.java

---
+++ thread.java/MultiThread.java
@@ -0,0 +1,64 @@
+public class MultiThread {
+    public static void main(String[] args) {
+        /*
+
+        Способ 1
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        t1.task = "Стираем белье";
+        t2.task ="Моем посуду";
+        t1.start();
+        t2.start();*/
+
+        /*
+        Способ 2
+        Thread t1 = new Thread(new MyRunnableClass("Моем посуду"));
+        Thread t2 = new Thread(new MyRunnableClass("Стираем белье"));
+        t1.start();
+        t2.start();*/
+        
+        Thread t1 = new Thread(new Runnable() {
+      
+            @Override
+            public void run() {
+                for (int i = 0; i < 200; i++) {
+                    System.out.println(i+" стираем белье");
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 200; i++) {
+                    System.out.println(i+" моем посуду");
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
+        class MyRunnableClass implements Runnable {
+            String task;
+
+            public MyRunnableClass(String ip, String task) {
+                this.task = task;
+            }
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 200; i++) {
+                    System.out.println(i + " " + this.task);
+                }
+            }
+        }
+        class MyThread implements Runnable {
+            String task;
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 200; i++) {
+                    System.out.println(i + " " + this.task);
+                }
+            }
+      
